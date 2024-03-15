97a69ae21a2978ccc63605e1f1910d02b82e8465
Job Kilonzo <jobkilonzo95@gmail.com>
2024-03-15 18:37:54 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        SUV fortuner = new SUV("Fortuner", false);
+        fortuner.move(40, 2);
+        fortuner.accelerate(20);
+    }
+}
